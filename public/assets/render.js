f5b991bb1bd7b06aaf2fa3c247a343a381e5735c
Felipe Bello Dultra <felipe.dultra@etec.sp.gov.br>
2020-01-11 16:41:07 -0300

    Little changes

--- public/assets/render.js
+++ public/assets/render.js
@@ -1,37 +1,37 @@
 var socket = io('http://localhost:3000');
 
 function renderMessages(message) {
-    $('.messages').append('<div class="message"><strong>' + message.author + '</strong>: ' + message.message + '</div>')
+  $('.messages').append('<div class="message"><strong>' + message.author + '</strong>: ' + message.message + '</div>')
 };
 
-socket.on('previousMessage', function(messages) {
-    for (showPreviousMessage of messages) {
-        renderMessages(showPreviousMessage);
-    }
+socket.on('previousMessage', function (messages) {
+  for (showPreviousMessage of messages) {
+    renderMessages(showPreviousMessage);
+  }
 })
 
-socket.on('receiveMessage', function(message) {
-    renderMessages(message)
+socket.on('receiveMessage', function (message) {
+  renderMessages(message)
 });
 
 $('#chat').submit(function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var author = $('input[name=username]').val();
-    var message = $('input[name=message]').val();
+  var author = $('input[name=username]').val();
+  var message = $('input[name=message]').val();
 
-    if (author.length && message.lenght != '') {
-        var messageObject = {
-            author: author,
-            message: message,
-        };
+  if (author.length && message.lenght != '') {
+    var messageObject = {
+      author: author,
+      message: message,
+    };
 
-        renderMessages(messageObject);
+    renderMessages(messageObject);
 
-        socket.emit('sendMessage', messageObject);
-    } else {
-        alert('Informe uma mensagem!');
-    }
+    socket.emit('sendMessage', messageObject);
+  } else {
+    alert('Informe uma mensagem!');
+  }
 
-    message = $('input[name=message]').val('');
+  message = $('input[name=message]').val('');
 });
--- server.js
+++ server.js
@@ -10,21 +10,19 @@ app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', (req, res) => {
-    res.render('index.html');
-});
+app.use('/', (req, res) => res.render('index.html'));
 
 let messages = [];
 
 io.on('connection', socket => {
-    console.log(`Socket Conectado: ${socket.id}`);
+  console.log(`Socket Conectado: ${socket.id}`);
 
-    socket.emit('previousMessage', messages);
+  socket.emit('previousMessage', messages);
 
-    socket.on('sendMessage', data => {
-        messages.push(data);
-        socket.broadcast.emit('receiveMessage', data);
-    })
+  socket.on('sendMessage', data => {
+    messages.push(data);
+    socket.broadcast.emit('receiveMessage', data);
+  })
 });
 
-server.listen(3000);
+server.listen(3000, () => console.log('Server Running'));
